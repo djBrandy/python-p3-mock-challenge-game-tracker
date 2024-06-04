9e280e656b48d05e9e0191dced8230961c7a6bc0
brandondando <dandobrandon0@gmail.com>
2024-06-04 06:34:03 +0300

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,8 @@ from classes.many_to_many import Player
 from classes.many_to_many import Game
 from classes.many_to_many import Result
 
+
+
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
